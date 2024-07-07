508f6e8e678b3372a7be951320ae0b3a95514765
kogupta <kohinoor.gupta@gmail.com>
2024-07-08 03:42:26 +0530

    ch6 parser

--- src/main/java/org/kogu/lox/ch5_ast/UnaryOperator.java
+++ src/main/java/org/kogu/lox/ch5_ast/UnaryOperator.java
@@ -1,9 +1,19 @@
 package org.kogu.lox.ch5_ast;
 
+import org.kogu.lox.ch4_scanning.TokenType;
+
 public enum UnaryOperator {
     Negative("-"),
     Not("!");
 
     public final String symbol;
     UnaryOperator(String symbol) {this.symbol = symbol;}
+
+    public static UnaryOperator from(TokenType tokenType) {
+        return switch (tokenType) {
+            case MINUS -> Negative;
+            case BANG -> Not;
+            default -> throw new IllegalArgumentException("Unknown token type: " + tokenType);
+        };
+    }
 }
--- src/main/java/org/kogu/lox/ch_parser/Parser.java
+++ src/main/java/org/kogu/lox/ch_parser/Parser.java
@@ -4,6 +4,7 @@ import org.kogu.lox.ch4_scanning.Token;
 import org.kogu.lox.ch4_scanning.TokenType;
 import org.kogu.lox.ch5_ast.BinaryOperator;
 import org.kogu.lox.ch5_ast.Expr;
+import org.kogu.lox.ch5_ast.UnaryOperator;
 
 import java.util.List;
 
@@ -27,6 +28,80 @@ public final class Parser {
             Token op = previous();
             Expr right = comparison();
             expr = Expr.binary(expr, BinaryOperator.from(op.tokenType()), right);
+        }
+
+        return expr;
+    }
+
+    // comparison -> term ( ( ">" | ">=" | "<" | "<=" ) term )* ;
+    private Expr comparison() {
+        Expr expr = term();
+        while (matchAny(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+            Token op = previous();
+            Expr right = term();
+            expr = Expr.binary(expr, BinaryOperator.from(op.tokenType()), right);
+        }
+
+        return expr;
+    }
+
+    // term -> factor ( ( "-" | "+" ) factor )* ;
+    private Expr term() {
+        Expr expr = factor();
+        while (matchAny(MINUS, PLUS)) {
+            Token op = previous();
+            Expr right = factor();
+            expr = Expr.binary(expr, BinaryOperator.from(op.tokenType()), right);
+        }
+
+        return expr;
+    }
+
+    // factor -> unary ( ( "/" | "*" ) unary )* ;
+    private Expr factor() {
+        Expr expr = unary();
+        while (matchAny(SLASH, STAR)) {
+            Token op = previous();
+            Expr right = unary();
+            expr = Expr.binary(expr, BinaryOperator.from(op.tokenType()), right);
+        }
+
+        return expr;
+    }
+
+    // unary -> ( "!" | "-" ) unary | primary
+    private Expr unary() {
+        if (matchAny(BANG, MINUS)) {
+            Token op = previous();
+            Expr expr = primary();
+            return Expr.unary(UnaryOperator.from(op.tokenType()), expr);
+        }
+
+        return primary();
+    }
+
+    // primary -> NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")" ;
+    private Expr primary() {
+        if (matchAny(TRUE)) return Expr.literal(true);
+        if (matchAny(FALSE)) return Expr.literal(false);
+        if (matchAny(NIL)) return Expr.nil();
+
+        if (matchAny(STRING, NUMBER)) {
+            Token token = previous();
+            if (token instanceof Token.LiteralToken (_, _, Object literal, _)) {
+                return switch (literal) {
+                    case String s -> Expr.literal(s);
+                    case Integer i -> Expr.literal(i);
+                    case Double v -> Expr.literal(v);
+                    case null, default -> throw new IllegalArgumentException("Unrecognized literal: " + literal);
+                };
+            }
+        }
+
+        if (matchAny(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
         }
     }
 
